2b35568fe4f8a6af8c0f22b455e90c71275337fc
Awdhesh <awdheshdubey64@gmail.com>
2024-04-10 10:29:34 +0530

    By Awdhesh Dubey

--- lib/features/home/screens/home_page.dart
+++ lib/features/home/screens/home_page.dart
@@ -47,6 +47,94 @@ import 'package:baderia_tech_wave/common/widgets/appbar/appbar.dart';
 import 'package:baderia_tech_wave/features/home/screens/profile_card/profile_box.dart';
 import 'package:baderia_tech_wave/utils/constants/colors.dart';
 import '../../../Circular Menus/circular_menus_with_circle.dart';
+import '../widgets/animation_switcher/hide_show_animation.dart';
+import 'left_circle_menu_and_screens/fees/fees_card/fees_card_contanier.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int selectedIndex = -1;
+  bool showFeesCard = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //backgroundColor: const Color(0xFFFFE0E5),
+
+      // backgroundColor: EColors.backgroundColor,
+      appBar: GAppBar(
+        showBackArrow: false,
+        title: Image.asset(
+          'assets/logo/global_college_logo.png',
+          width: 105,
+        ),
+        backgroundColor: EColors.primary,
+        centerTitle: true,
+      ),
+
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            CircularMenuWithCircle(
+              onIndexChanged: (index) {
+                setState(() {
+                  selectedIndex = index;
+                  toggleFeesCardAndProfileBox();
+                });
+              },
+            ),
+            AnimatedSwitcherWidget(
+              firstWidget: FeesCard(),
+              secondWidget: const ProfileBox(),
+              showFirst: showFeesCard,
+              selectedIndex: selectedIndex,
+            ),
+            // SizedBox(height: 10 ,),
+
+            // Container(
+            //   // width: 400,
+            //   height: 200,
+            //   decoration: BoxDecoration(
+            //     image: DecorationImage(
+            //       image: AssetImage("assets/Backgrounds/back11.png"),
+            //       fit: BoxFit.contain,
+            //       opacity: .1,
+            //       // alignment: FractionalOffset(0, 1.4),
+            //     ),
+            //   ),
+            // ),
+            //
+          ],
+        ),
+      ),
+    );
+  }
+
+  void toggleFeesCardAndProfileBox() {
+    setState(() {
+      // Toggle between FeesCard and ProfileBox for index 0 or 10, hide for others
+      if (selectedIndex == 0 || selectedIndex == 10) {
+        showFeesCard = !showFeesCard;
+      }
+    });
+  }
+}
+
+
+
+/*
+import 'package:flutter/material.dart';
+import 'package:baderia_tech_wave/common/widgets/appbar/appbar.dart';
+import 'package:baderia_tech_wave/features/home/screens/profile_card/profile_box.dart';
+import 'package:baderia_tech_wave/utils/constants/colors.dart';
+import '../../../Circular Menus/circular_menus_with_circle.dart';
 import '../../../data/api/api_services.dart';
 import '../widgets/animation_switcher/hide_show_animation.dart';
 import 'left_circle_menu_and_screens/fees/fees_card/fees_card_contanier.dart';
@@ -128,6 +216,8 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+
+ */
 
 
 
